2f985cefde18e977c7e83829b286f8cc7d300e2c
Pulak Bala <pulakbala0524@gmail.com>
2022-12-31 02:47:36 +0600

    category id set

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { route } from './Route/Route/Route';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={route}></RouterProvider>
     </div>
   );
