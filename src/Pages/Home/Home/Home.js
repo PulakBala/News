c50641c5ff4caf5ac2546ce34cc54042093c94f5
Pulak Bala <pulakbala0524@gmail.com>
2023-01-11 03:27:35 +0600

    home component title add

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import NewSummeryCard from '../../Shared/NewSummeryCard/NewSummeryCard';
 
 const Home = () => {
+    const allnews = useLoaderData();
     return (
         <div>
-            <h2>This is home component</h2>
+            <h2>This is home component {allnews.length}</h2>
+            {
+                allnews.map(news =><NewSummeryCard
+                    key={news._id}
+                    news={news}
+                ></NewSummeryCard>)
+            }
         </div>
     );
 };
