e10c6f095bbea24ddaa85a61eeaa3ce1d7929412
Pulak Bala <pulakbala0524@gmail.com>
2023-01-19 01:07:01 +0600

    added vercel server

--- src/Route/Route/Route.js
+++ src/Route/Route/Route.js
@@ -17,17 +17,17 @@ export const route = createBrowserRouter([
             {
                 path: '/',
                 element: <Home></Home>,
-                loader: () => fetch('http://localhost:5000/news')
+                loader: () => fetch('https://dragon-news-server-six-gilt.vercel.app/news')
             },
             {
                 path: '/category/:id',
                 element: <Category></Category>,
-                loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
+                loader: ({params}) => fetch(`https://dragon-news-server-six-gilt.vercel.app/category/${params.id}`)
             },
             {
                 path: '/news/:id',
                 element: <PrivateRoute><News></News></PrivateRoute>,
-                loader:({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+                loader:({params}) => fetch(`https://dragon-news-server-six-gilt.vercel.app/news/${params.id}`)
             },
             {
                 path: '/login',
